cfbfd23ef8e7c1275a4adf517f699778b784def1
Pablo Hernán Gómez <175459414+PabloGomez05@users.noreply.github.com>
2024-11-10 22:17:11 -0300

    Header terminado(sin responsive aun)

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import { src, dest, watch } from 'gulp';
+
+const sass = gulpSass(dartSass);
+
+export function css(done) { 
+
+    src('src/scss/app.scss')
+        .pipe( sass().on('error', sass.logError) )
+        .pipe(dest('dist/css'));
+
+    done();
+}
+
+export function dev () {
+
+    watch( 'src/scss/**/*.scss', css );
+}
